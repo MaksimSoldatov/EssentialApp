4f9eb7d9fdde25c9054223be1a58244eb9548705
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-03 10:06:57 +0300

    'RemoteFeedLoader' does not request data upon creation

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Maxim Soldatov on 12/3/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
