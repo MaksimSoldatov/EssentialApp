d9385d387146503dc1bd6a2fa27ad00b51140081
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-05 15:41:43 +0300

    Move 'LocalFeedItem' to its own file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,20 @@
+//
+//  Created by Maksim Soldatov on 12/5/20.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
