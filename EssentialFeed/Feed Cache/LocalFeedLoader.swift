1effc203380c59e010292ac11434654a1483c866
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-05 15:04:42 +0300

    Add SaveResult type alliance to protect code from potential tracking changes

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -5,6 +5,8 @@ import Foundation
 
 public final class LocalFeedLoader {
     
+    public typealias SaveResult = Error?
+    
     private let store: FeedStore
     private let currentDate: () -> Date
     
@@ -13,7 +15,7 @@ public final class LocalFeedLoader {
         self.currentDate = currentDate
     }
     
-    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+    public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] error in
             guard let self = self else { return }
             
@@ -25,7 +27,7 @@ public final class LocalFeedLoader {
         }
     }
     
-    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+    private func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
         store.insert(items, timestamp: currentDate()) { [weak self] error in
             guard self != nil else { return }
             completion(error)
