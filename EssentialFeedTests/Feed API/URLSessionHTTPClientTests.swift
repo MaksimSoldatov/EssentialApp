65c2b1984fe71c3d3d4b18742d783ce93f239d91
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-06 12:52:39 +0300

    Retrieving from empty cache twice delivers same empty result (no side-effects)

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -35,9 +35,10 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     func test_getFromURL_failsOnRequestError()  {
         let requestError = anyNSError()
+        
         let receivedError = resultErrorFor(data: nil, response: nil, error: requestError)
         
-        XCTAssertEqual(receivedError as NSError?, requestError)
+        XCTAssertNotNil(receivedError)
     }
     
     func test_getFromURL_failsOnAllNilValues() {
