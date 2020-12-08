2586892a582a969184855032c127786bc9233539
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-08 14:37:03 +0300

    Extract UIButton test helper to a separate file

---
+++ EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Maksim Soldatov on 8.12.20.
+//
+import UIKit
+
+extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
