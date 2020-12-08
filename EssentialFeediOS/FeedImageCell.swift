558d3952163dc796d0fbdb30e5d4f554a0492930
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-08 10:06:20 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Maksim Soldatov on 7.12.20.
+//
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    
+}
