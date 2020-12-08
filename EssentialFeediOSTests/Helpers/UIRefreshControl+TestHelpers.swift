140ec0128387d486d2bf88977aed820cfcad01b5
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-08 14:35:59 +0300

    Extract UIRefreshControl test helper to a separate file

---
+++ EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Maksim Soldatov on 8.12.20.
+//
+import UIKit
+
+extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
