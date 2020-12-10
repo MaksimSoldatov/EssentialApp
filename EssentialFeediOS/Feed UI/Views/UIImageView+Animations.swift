33ddfb33a594983b2356062b92373a8f114e2740
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-10 16:30:46 +0300

    Display image with animation

---
+++ EssentialFeediOS/Feed UI/Views/UIImageView+Animations.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Maksim Soldatov on 10.12.20.
+//
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
