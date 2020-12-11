82aa781defb14a6ae051248b90c6d9034a24ad21
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-11 15:10:14 +0300

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/Models/FeedErrorViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Maksim Soldatov on 11.12.20.
+//
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
