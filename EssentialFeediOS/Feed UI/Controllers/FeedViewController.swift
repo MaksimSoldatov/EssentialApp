a5ac163471b2e7582f7dbe28e468cdd22b93b801
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-11 13:34:47 +0300

    Extract refreshing logic to an extension

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -54,11 +54,7 @@ public final class FeedViewController: UITableViewController {
 extension FeedViewController: FeedLoadingView {
     
     func display(_ viewModel: FeedLoadingViewModel) {
-        if viewModel.isLoading {
-            refreshControl?.beginRefreshing()
-        } else {
-            refreshControl?.endRefreshing()
-        }
+        refreshControl?.update(isRefreshing: viewModel.isLoading)
     }
 }
 
---
+++ EssentialFeediOS/Feed UI/Views/Helpers/UIRefreshControl+Helpers.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Maksim Soldatov on 11.12.20.
+//
+import UIKit
+
+extension UIRefreshControl {
+    func update(isRefreshing: Bool) {
+        isRefreshing ? beginRefreshing() : endRefreshing()
+    }
+}
