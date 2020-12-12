29da6752eece4503bb40003cfd59c73ebf95f219
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-12 13:37:29 +0300

    Move `FeedImagePresenter` and collaborators to production

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -4,64 +4,6 @@
 import XCTest
 import EssentialFeed
 
-public struct FeedImageViewModel<Image> {
-    let description: String?
-    let location: String?
-    let image: Image?
-    let isLoading: Bool
-    let shouldRetry: Bool
-    
-    var hasLocation: Bool {
-        return location != nil
-    }
-}
-
-public protocol FeedImageView {
-    associatedtype Image
-    
-    func display(_ model: FeedImageViewModel<Image>)
-}
-
-public final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
-    
-    private let view: View
-    private let imageTransformer: (Data) -> Image?
-    
-    init(view: View, imageTransformer: @escaping (Data) -> Image?) {
-        self.view = view
-        self.imageTransformer = imageTransformer
-    }
-    
-    func didStartLoadingImageData(with data: Data, for model: FeedImage) {
-        let image = imageTransformer(data)
-        view.display(FeedImageViewModel(
-                        description: model.description,
-                        location: model.location,
-                        image: image,
-                        isLoading: true,
-                        shouldRetry: false))
-    }
-    
-    func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
-        let image = imageTransformer(data)
-        view.display(FeedImageViewModel(
-                        description: model.description,
-                        location: model.location,
-                        image: image,
-                        isLoading: false,
-                        shouldRetry: image == nil))
-    }
-    
-    func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
-        view.display(FeedImageViewModel(
-                        description: model.description,
-                        location: model.location,
-                        image: nil,
-                        isLoading: false,
-                        shouldRetry: true))
-    }
-}
-
 class FeedImagePresenterTests: XCTestCase {
     
     func test_init_doesNotSendMessagesToView() {
