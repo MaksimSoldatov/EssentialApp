f8cc1d16486711340e33a9712636a9db3f585ff3
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-12 13:38:46 +0300

    Move `FeedImageViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/Models/FeedImageViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Maksim Soldatov on 12.12.20.
+//
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
