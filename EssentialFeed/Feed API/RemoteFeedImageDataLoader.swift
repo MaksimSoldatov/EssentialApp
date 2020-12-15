719cea616404ae7526f7a77f212a928cf94607b4
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-15 20:36:44 +0300

    Load `NSManagedObjectModel` instance lazily and cache it to prevent multiple `NSEntityDescriptions` claiming the same `NSManagedObject` model subclasses (This problem just generates warnings but could lead to undefined behaviour in the future).

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -16,6 +16,21 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         case invalidData
     }
     
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            task.complete(with: result
+                            .mapError{ _ in Error.connectivity}
+                            .flatMap { (data, response) in
+                                let isValidResponse = response.isOK && !data.isEmpty
+                                return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                            })
+        }
+        return task
+    }
+    
     private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
         private var completion: ((FeedImageDataLoader.Result) -> Void)?
         
@@ -39,18 +54,5 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         }
     }
     
-    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = HTTPClientTaskWrapper(completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            task.complete(with: result
-                            .mapError{ _ in Error.connectivity}
-                            .flatMap { (data, response) in
-                                let isValidResponse = response.isOK && !data.isEmpty
-                                return isValidResponse ? .success(data) : .failure(Error.invalidData)
-                            })
-        }
-        return task
-    }
+
 }
