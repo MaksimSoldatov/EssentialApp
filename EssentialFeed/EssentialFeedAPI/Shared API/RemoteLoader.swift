710000bed4c75c300e6ad43dfe682b0f5f54b871
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-22 18:31:04 +0300

    Duplicate 'RemoteFeedLoader' as 'FeedLoader'

---
+++ EssentialFeed/EssentialFeedAPI/Shared API/RemoteLoader.swift
@@ -0,0 +1,45 @@
+//
+//  Created by Maksim Soldatov on 22.12.20.
+//
+import Foundation
+import EssentialFeed
+
+public final class RemoteLoader: FeedLoader {
+    
+    private let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public typealias Result = FeedLoader.Result
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            switch result {
+            case let .success((data, response)):
+                completion(RemoteLoader.map(data, from: response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+    
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, from: response)
+            return .success(items)
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+}
