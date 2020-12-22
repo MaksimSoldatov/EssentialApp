5078b93464b1927fefb11e2a57c93949d049c2a7
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-22 17:45:18 +0300

    Add ImageComment data model

---
+++ EssentialFeed/EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,19 @@
+//
+//  Created by Maksim Soldatov on 22.12.20.
+//
+import Foundation
+
+public struct ImageComment: Hashable {
+    
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
