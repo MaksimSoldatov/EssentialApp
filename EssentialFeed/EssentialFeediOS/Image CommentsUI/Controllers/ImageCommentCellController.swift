61d02658a3540bd6c45f3f561482dd65286c0823
Maxim Soldatov <maxim.soldatov@me.com>
2021-01-01 14:12:59 +0300

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image CommentsUI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,30 @@
+//
+//  Created by Maksim Soldatov on 1.01.21.
+//
+import UIKit
+import EssentialFeedPresentation
+
+public class ImageCommentCellController: CellController {
+    
+    private let model: ImageCommentViewModel
+    
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+    
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.messageLabel.text = model.message
+        cell.usernameLabel.text = model.username
+        cell.dateLabel.text = model.date
+        return cell
+    }
+    
+    public func preload() {
+        
+    }
+    
+    public func cancelLoad() {
+        
+    }
+}
