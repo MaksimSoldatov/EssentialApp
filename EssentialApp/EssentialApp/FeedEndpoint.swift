17cae2e1f6b0ef7adf5239436c65466369f000a9
Maxim Soldatov <maxim.soldatov@me.com>
2021-01-12 13:31:25 +0300

    Set FeedEndpoint limit to 10

---
+++ EssentialApp/EssentialApp/FeedEndpoint.swift
@@ -0,0 +1,22 @@
+//
+//  Created by Maksim Soldatov on 10.01.21.
+//
+import Foundation
+
+public enum FeedEndpoint {
+    case get
+    
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case .get:
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = baseURL.path + "/v1/feed"
+            components.queryItems = [
+                URLQueryItem(name: "limit", value: "10")
+            ]
+            return components.url!
+        }
+    }
+}
